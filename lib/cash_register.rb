9b39bf6a9789892cf988c2c8886a8fc59b09f512
Taylor Donlan <taylordonlan@gmail.com>
2018-03-12 18:25:39 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,8 +20,8 @@ class CashRegister
 
   def apply_discount
     if discount != 0
-      #fractional_discount = @discount/100.00
-      @total *= @discount
+      fractional_discount = (@discount/100.00)
+      @total *= fractional_discount
       return "After the discount, the total comes to $#{self.total}."
     else
       return "There is no discount to apply."
