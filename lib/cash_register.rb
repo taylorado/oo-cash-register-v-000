74a52ee831008159ce96ddc5571c9b07f725be04
Taylor Donlan <taylordonlan@gmail.com>
2018-03-12 18:33:40 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,8 +2,6 @@
 class CashRegister
   attr_accessor :discount, :last_transaction, :total
 
-
-
   def initialize(discount = 0)
     @items = []
     @discount = discount
@@ -16,8 +14,6 @@ class CashRegister
     number.times{@items.push(name)}
   end
 
-
-
   def apply_discount
     if discount != 0
       @total -= @total*(@discount/100.00)
@@ -25,24 +21,14 @@ class CashRegister
     else
       return "There is no discount to apply."
     end
-  # the cash register was initialized with an employee discount:
-    # applies the discount to the total price
-    # returns success message with updated total
-    # reduces the total
-
-  # the cash register was *NOT* initialized with an employee discount:
-    # returns a string error message that there is no discount to appply
   end
 
   def items
     return @items
-  # (self).items!?
-  # returns an array containing all items that have been added
   end
 
   def void_last_transaction
     self.total -= @last_transaction
-  # subtracts the last transaction from the total
   end
 
 end
