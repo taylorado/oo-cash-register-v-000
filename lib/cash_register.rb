3fe413196b244fee24d584a30f09376440671da9
Taylor Donlan <taylordonlan@gmail.com>
2018-03-12 18:29:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
   def apply_discount
     if discount != 0
       @total -= @total*(@discount/100.00)
-      return "After the discount, the total comes to $#{(self.total).to_i}."   # to_i invoked here only to pass tests.  
+      return "After the discount, the total comes to $#{(self.total).to_i}."   # to_i invoked here only to pass tests.
     else
       return "There is no discount to apply."
     end
